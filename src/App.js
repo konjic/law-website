396093bfd794536859c7a0f2776784e9b5056549
Burloiu Razvan <razvanburloiu@gmail.com>
2022-06-01 19:10:49 +0300

    linkin seo2

--- src/App.js
+++ src/App.js
@@ -30,9 +30,9 @@ function App() {
             />
 
 <meta property='og:title' content='Title of the article'/>
-<meta property='og:image' content='//media.example.com/ 1234567.jpg'/>
+<meta property='og:image' content='./Media/lawyer_women.png'/>
 <meta property='og:description' content='Description that will show in the preview'/>
-<meta property='og:url' content='//www.example.com/URL of the article'/>
+<meta property='og:url' content='https://law-website-eight.vercel.app/'/>
           </Helmet>
           <Routes>
             <Route path='/' element={<Home/>} />
