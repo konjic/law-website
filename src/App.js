725263a415b85bc5f828264fb76ab59395859bcc
Burloiu Razvan <razvanburloiu@gmail.com>
2022-06-01 19:28:08 +0300

    linkin seo2

--- src/App.js
+++ src/App.js
@@ -25,24 +25,11 @@ function App() {
           <Helmet>
             <title>VERY COOL LAW FIRM WEBSITE</title>
             
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="description" key="description" content="woooow" />
-      <meta name="title" key="title" content='VERY COOL LAW FIRM WEBSITE' />
-      <meta property="og:title" key="og:title" content='VERY COOL LAW FIRM WEBSITE' />
-      <meta property="og:locale" key="og:locale" content="en_US" />
-      <meta charSet="utf-8" />
-      <meta property="og:type" key="og:type" content="website" />
-      <meta
-        property="og:description"
-        key="og:description"
-        content="woooow"
-      />
-      <meta
-        property="og:image"
-        key="og:image"
-        content="https://images.squarespace-cdn.com/content/v1/602334ccc83cc72486df6be3/1613162047924-M4ZJZ3N7HCMH4HXSUJ68/TMLF-0101+Brand+Identity_LOGO-black.png"
-      />  
+            <meta property="og:type" content="website"/>
+<meta property="og:url" content="https://law-website-eight.vercel.app/"/>
+<meta property="og:title" content="Test"/>
+<meta property="og:description" content="WOW"/>
+<meta property="og:image" content=""/> 
           </Helmet>
           <Routes>
             <Route path='/' element={<Home/>} />
